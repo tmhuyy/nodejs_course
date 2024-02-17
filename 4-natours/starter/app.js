485a8a89c2513fe79c66ca8d99773c3d8c8f9dc4
huy.tranminh <tmhuyy.itiu@gameloft.com>
2024-02-17 11:00:27 +0700

    + set up env variable

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,7 +9,12 @@ const userRouter = require("./routes/userRoutes")
 const testRouter = require("./routes/testRoutes")
 
 // -------------------------------MIDDLEWARE ----------------------------------------------------
-app.use(morgan("dev"))
+if (process.env.NODE_ENV === "development")
+{
+    app.use(morgan("dev"))
+}
+
+app.use(express.static(`${__dirname}/public`))
 
 app.use(express.json()); // express.json(): middleware modifies incoming request data
 
