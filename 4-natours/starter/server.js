2239452f957fb7fa9ee306ca76ac9fb6abd4052c
huy.tranminh <tmhuyy.itiu@gameloft.com>
2024-02-19 14:56:46 +0700

    - update

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,34 @@
+const mongoose = require("mongoose")
 require('dotenv').config()
+
+const URI = process.env.DB_LINK.replace("<DB_PASSWORD>", process.env.DB_PASSWORD)
+mongoose.connect(URI).then(() =>
+{
+    console.log("DB CONNECTION IS SUCCESSFUL")
+})
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A tour must have a name"],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price"]
+    }
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+
+
+
+
 const app = require("./app")
 
 const PORT = process.env.PORT || 3000;
